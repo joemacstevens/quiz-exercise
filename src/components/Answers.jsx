bd9260e3fd0cc0cc4b3ac84e1a3bac4f181c277b
Joe Stevens <joemac79@gmail.com>
2022-05-25 11:20:40 -0400

    update time function

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,11 +1,30 @@
 /** @jsxImportSource @emotion/react */
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment, useEffect, useState, useRef } from 'react';
 import uuid from 'react-uuid';
+import { css } from '@emotion/react'
+import styled from '@emotion/styled';
+
+export const Timer = styled('div')`
+position: absolute;
+z-index: 10;
+top: 130px;
+height: 40px;
+width: 35vw;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+export const Countdown = styled('div')`
+  font-size: 2em;`;
 
 export const Answers = (props) => {
     const [options, setOptions] = useState([]);
     const [answered, setAnswered] = useState(false);
     const [result, setResult] = useState(false);
+    const [counter, setCounter] = useState(90);
+
+    const intervalRef = useRef();
+    intervalRef.current = counter
 
     useEffect(() => {
         setOptions(props.options);
@@ -26,7 +45,7 @@ export const Answers = (props) => {
     }
 
     const CheckButton = (event) => {
-        if(answered) {
+        if (answered) {
             return <button onClick={handleNext}>Next</button>
         } else {
             return <button onClick={handleClick}>Check</button>
@@ -34,28 +53,59 @@ export const Answers = (props) => {
     }
 
     const Result = () => {
-        if(answered && result) {
+        if (answered && result) {
             return <div>Correct</div>
-        } else if(answered && !result) {
+        } else if (answered && !result) {
             return <div>Incorrect</div>
         } else {
             return null
         }
     }
 
+    useEffect(() => {
+        setCounter(10)
+        const _timer = setInterval(() => {
+            if (intervalRef.current == 0) {
+                clearInterval(_timer);
+                handleClick();
+                
+            } else {
+                setCounter(intervalRef.current - 1);
+            }
+        }, 1000);
+        return () => clearInterval(_timer); // clean up 
+    }, [props.options]);
+
     return (
+        <Fragment>
+            <Timer>
+                <div css={css`position:absolute`}>{counter}</div>
+                <svg css={css`width: 60px;
+height: 60px;
+margin-top: 0;`}>
+                    <circle css={css`  stroke-dasharray: 203px;
+stroke-dashoffset: 0px;
+stroke-linecap: round;
+stroke-width: 5px;
+stroke: white;
+fill: white;`} r="25" cx="30" cy="30">
 
-        <div >
-            {options.map((item, index) => {
-                if (props.questionType === "multiple") {
-                    return <p><input disabled={answered} onChange={handleChange} checked={props.selectedAnswer.includes(item)} type="checkbox" value={item} key={uuid()} name={props.qid} />{item} </p>
-                } else {
-                    return <p><input disabled={answered} onChange={handleChange} checked={props.selectedAnswer.includes(item)} type="radio" value={item} name={props.qid} key={uuid()} />{item}</p>
-                }
+                    </circle>
+                </svg>
 
-            })}
-            <div><CheckButton /></div>
-            <div><Result /></div>
-        </div>
+            </Timer>
+            <div >
+                {options.map((item, index) => {
+                    if (props.questionType === "multiple") {
+                        return <p><input disabled={answered} onChange={handleChange} checked={props.selectedAnswer.includes(item)} type="checkbox" value={item} key={uuid()} name={props.qid} />{item} </p>
+                    } else {
+                        return <p><input disabled={answered} onChange={handleChange} checked={props.selectedAnswer.includes(item)} type="radio" value={item} name={props.qid} key={uuid()} />{item}</p>
+                    }
+
+                })}
+                <div><CheckButton /></div>
+                <div><Result /></div>
+            </div>
+        </Fragment>
     );
 }
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -5,28 +5,13 @@ import { QuestionHeading } from './Quiz';
 import { css } from '@emotion/react'
 import styled from '@emotion/styled';
 
-export const Timer = styled('div')`
-position: absolute;
-z-index: 10;
-margin-top: -22px;
-height: 40px;
-width: 35vw;
-display: flex;
-justify-content: center;
-align-items: center;
-`
-export const Countdown = styled('div')`
-  font-size: 2em;`;
+
 
 export const Question = (props) => {
   const { question, onUpdate, count } = props;
   const [answer, setAnswer] = useState([]);
   const [questionType, setQuestionType] = useState('single');
-  const [counter, setCounter] = useState(90);
 
-  const intervalRef = useRef();
-  intervalRef.current = counter
- 
   const handleAnswer = (option) => {
     if (questionType === 'multiple') {
       if (answer.includes(option)) {
@@ -40,6 +25,11 @@ export const Question = (props) => {
   }
 
   const checkAnswer = () => {
+    
+    if (answer.length !== question.correct_answer.length) {
+      return false;
+    } 
+
     const correct = answer.every(item => {
       return question.correct_answer.includes(item);
     })
@@ -62,37 +52,9 @@ export const Question = (props) => {
     }
     setAnswer([]);
   }, [question]);
-useEffect(() => {
-  setCounter(90)
-  const _timer = setInterval(() => {
-    if (intervalRef.current == 0) {
-      clearInterval(_timer);
-    } else {
-      setCounter(intervalRef.current - 1);
-    }
-  }, 1000);
-  return () => clearInterval(_timer); // clean up 
-}, [props.count])
 
   return (
     <Fragment>
-                  <Timer>
-            <div css={css`position:absolute`}>{counter}</div>
-              <svg css={css`width: 60px;
-    height: 60px;
-    margin-top: 0;`}>
-                <circle css={css`  stroke-dasharray: 203px;
-  stroke-dashoffset: 0px;
-  stroke-linecap: round;
-  stroke-width: 5px;
-  stroke: white;
-  fill: white;`} r="25" cx="30" cy="30">
-                 
-                </circle>
-              </svg>
-
-            </Timer>
-
       <QuestionHeading>
       <p>Question {count}/6</p> 
         <h3>{question.question}</h3></QuestionHeading>
